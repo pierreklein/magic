6de89123b72b930a4bda6031638952869c391f1d
pierreklein <me@pierrekle.in>
2017-08-14 11:04:35 -0700

    Update config.js
    newdash

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@ configReactDriveCms = {
     /**
      * Add the Id for the Google Sheets Dashboard file
      */
-    dashboardId: '1bLINCh7QVSQhzEw3WGsmTs39kP7MMx9n7BcpHxzNZqE',
+    dashboardId: '1h4C2qc2hoMUnZeh9KldzqVQAnrabI1jO7Aw0Ux47Bxc',
     /**
      * Add the Url Id for the Google Web App Script to forward email from the Contact Form
      */
